9ff0a6acf9e984eb284f62caa28ebe515280fc4f
Naveen <naveendurai19@gmail.com>
2025-03-05 18:35:42 +0530

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/formuladen/",
+
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
