57c9b5b4f453ba5a1ff5abd33517110bbed69859
Naveen <naveendurai19@gmail.com>
2025-03-05 18:31:12 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import { XIcon } from 'lucide-react';
 import { Button } from './components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from './components/ui/card';
